7e637529e99f9824b820bed021c9a817878c6d19
Ruta Zala <rutazala1889@gmail.com>
2024-08-22 17:25:21 +0530

    fix opn chain card design

--- src/pages/GoldenVisaOPNChain/OPNChainExploreSection.jsx
+++ src/pages/GoldenVisaOPNChain/OPNChainExploreSection.jsx
@@ -3,6 +3,7 @@ import img2 from "../../assets/OPNChain/digital-currency.png";
 import img3 from "../../assets/OPNChain/logo-sdk 1.png";
 import cradsBg from "../../assets/OPNChain/cards-bg.png";
 import CheckMark from "../../assets/OPNChain/CheckMark.png";
+import './opnChain.css'
 
 const cardData = [
   {
@@ -13,8 +14,8 @@ const cardData = [
       "Immutable Ledger",
       "Preventing double-spending",
     ],
-    gradient: "from-gray-900 to-gray-700",
     logo: img1,
+    className: "mb-[85px]"
   },
   {
     name: "EVM Compatibility",
@@ -24,8 +25,8 @@ const cardData = [
       "Immutable Ledger",
       "Preventing double-spending",
     ],
-    gradient: "from-gray-900 to-gray-800",
     logo: img2,
+    className: "mt-5"
   },
   {
     name: "Cosmos SDK",
@@ -35,10 +36,45 @@ const cardData = [
       "Immutable Ledger",
       "Preventing double-spending",
     ],
-    gradient: "from-indigo-500 to-indigo-400",
     logo: img3,
+    className: "mt-[85px]"
   },
 ];
+
+const OverlappingCards = () => {
+  return (
+    <div className="hidden lg:flex justify-around items-center h-[600px] relative opn-chain-explore-card-bg rounded-2xl mb-[120px]">
+        {cardData.map((card, index) => (
+          <div
+            key={index}
+            className={`transition-transform duration-500 ease-in-out  text-white-0  transform hover:scale-105 bg-transparent p-4 md:p-2 xl:p-8 ${card.className || ''} `}
+            style={{
+              zIndex: cardData.length - index,
+            }}
+          >
+            <div className="flex justify-center">
+              <img src={card.logo} className="h-10" />
+            </div>
+
+            <div className="text-3xl md:text-2xl lg:text-3xl text-center font-semibold my-6 ">
+              {card.name}
+            </div>
+            <ul className="space-y-4">
+              {card.benefits.map((benefit, i) => (
+                <li
+                  key={i}
+                  className="font-normal flex items-center gap-5 bg-white-0/20 md:px-2 xl:px-5 py-4 rounded-md"
+                >
+                  <img src={CheckMark} />
+                  {benefit}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </div>
+  );
+};
 
 const OPNChainExploreSection = () => {
   return (
@@ -48,16 +84,13 @@ const OPNChainExploreSection = () => {
         Explore an ecosystem where technology serves <br /> humanity & empowers
         people.
       </p>
-      <div className="flex flex-col items-center space-y-8 md:flex-row md:space-y-0 md:space-x-4 lg::space-x-8 relative">
-        {/* <img
-          src={cradsBg}
-          alt=""
-          className="absolute top-0 bottom-0 right-0 left-0 h-full w-full object-contain absolute"
-        /> */}
+      
+      <OverlappingCards/>
+      <div className="flex lg:hidden flex-col items-center space-y-8 md:flex-row md:space-y-0 md:space-x-4 lg::space-x-8 relative">
         {cardData.map((card, index) => (
           <div
             key={index}
-            className={`bg-gradient-to-b h-full ${card.gradient} text-white-0 p-4 md:p-2 lg:p-8 rounded-2xl shadow-lg w-full self-stretch z-10`}
+            className={` h-full opn-card${index+1} text-white-0 p-4 md:p-2 lg:p-8 rounded-2xl shadow-lg w-full self-stretch z-10`}
           >
             <div className="flex justify-center">
               <img src={card.logo} className="h-10" />
